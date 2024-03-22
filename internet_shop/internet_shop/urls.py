5448c9abe638b28236e66b945e8d4e106e1ff2bb
hanna <Xzeepchik@gmail.com>
2024-03-22 22:02:10 +0300

    main

---
+++ internet_shop/internet_shop/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('pages/', include('django.contrib.flatpages.urls')),
+]
