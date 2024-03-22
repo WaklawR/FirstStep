13e47525ea5b7f484f0b7948a03a5bb9993ad422
hanna <Xzeepchik@gmail.com>
2024-03-22 22:01:59 +0300

    main

---
+++ internet_shop/static/js/scripts.js
@@ -0,0 +1,32 @@
+/*!
+* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$(".toggle-password").click(function() {
+	  $(this).toggleClass("fa-eye fa-eye-slash");
+	  var input = $($(this).attr("toggle"));
+	  if (input.attr("type") == "password") {
+	    input.attr("type", "text");
+	  } else {
+	    input.attr("type", "password");
+	  }
+	});
+
+})(jQuery);
