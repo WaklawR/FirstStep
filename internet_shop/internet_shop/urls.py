64b1c24039a7f88ef0c453ecdf5013f2159941c5
hanna <Xzeepchik@gmail.com>
2024-03-23 11:29:34 +0300

    main

--- internet_shop/internet_shop/urls.py
+++ internet_shop/internet_shop/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pages/', include('django.contrib.flatpages.urls')),
+    path('', include('django.contrib.flatpages.urls')),
 ]
 
 
--- internet_shop/shop/models.py
+++ internet_shop/shop/models.py
@@ -3,10 +3,24 @@ from django.db import models
 class Category(models.Model):
     name=models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.IntegerField()
     stock = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} р."
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
